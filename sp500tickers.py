afa13645a958e58efadf1ad263d1ef1da6c0b7db
justusBersten <justusbersten@gmail.com>
2017-10-05 23:47:56 +0200

    First Commit sp500tickers.py

---
+++ sp500tickers.py
@@ -0,0 +1,30 @@
+## Script to crawl all sp500 ticker from wikipedia (returned as python dictionary)
+
+import urllib2
+from bs4 import BeautifulSoup
+
+
+SITE = "http://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+
+def scrape_list(site):
+    hdr = {'User-Agent': 'Mozilla/5.0'}
+    req = urllib2.Request(site, headers=hdr)
+    page = urllib2.urlopen(req)
+    soup = BeautifulSoup(page, "html.parser")
+
+    table = soup.find('table', {'class': 'wikitable sortable'})
+    sector_tickers = dict()
+    for row in table.findAll('tr'):
+        col = row.findAll('td')
+        if len(col) > 0:
+            sector = str(col[3].string.strip()).lower().replace(' ', '_')
+            ticker = str(col[0].string.strip())
+            if sector not in sector_tickers:
+                sector_tickers[sector] = list()
+            sector_tickers[sector].append(ticker)
+    return sector_tickers
+
+
+sector_tickers = scrape_list(SITE)
+
+print sector_tickers
